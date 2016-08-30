3250d1b51a3783c12dde9001fa31f96e9f4e6deb
stofstik <verstegen.daan@gmail.com>
2016-08-30 02:03:24 +0200

    Sane naming

--- app/src/main/java/com/safe/myapp/SafeRestClient.java
+++ app/src/main/java/com/safe/myapp/SafeRestClient.java
@@ -1,5 +1,6 @@
 package com.safe.myapp;
 
+import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.SyncHttpClient;
@@ -9,18 +10,18 @@ public class SafeRestClient {
     private static final int TIMEOUT = 30 * 1000;
 
 
-    private static SyncHttpClient syncHttpClientlient = new SyncHttpClient(SafeService.HTTP_PORT);
+    private static SyncHttpClient httpClient = new SyncHttpClient(SafeService.HTTP_PORT);
 
     public static void syncGet(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        syncHttpClientlient.setTimeout(TIMEOUT);
-        syncHttpClientlient.setMaxRetriesAndTimeout(1, 1000);
-        syncHttpClientlient.get(getAbsoluteUrl(url), params, responseHandler);
+        httpClient.setTimeout(TIMEOUT);
+        httpClient.setMaxRetriesAndTimeout(1, 1000);
+        httpClient.get(getAbsoluteUrl(url), params, responseHandler);
     }
 
     public static void syncPost(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
-        syncHttpClientlient.setTimeout(TIMEOUT);
-        syncHttpClientlient.setMaxRetriesAndTimeout(1, 1000);
-        syncHttpClientlient.post(getAbsoluteUrl(url), params, responseHandler);
+        httpClient.setTimeout(TIMEOUT);
+        httpClient.setMaxRetriesAndTimeout(1, 1000);
+        httpClient.post(getAbsoluteUrl(url), params, responseHandler);
     }
 
     private static String getAbsoluteUrl(String relativeUrl) {
